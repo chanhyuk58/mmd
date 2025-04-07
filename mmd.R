bfb124ae2aca6dee5d24b1993f4d5d300f92fb33
Chanhyuk Park <chan.hyuk.58@gmail.com>
2025-04-07 14:02:38 -0500

    2025-04-07 14:02

--- mmd.R
+++ mmd.R
@@ -1,7 +1,12 @@
 library('np')
 library('parallel')
-library('pracma')
-library('HDCD')
+# library('pracma')
+# library('HDCD')
+library('readr')
+library('dplyr')
+
+path <- getwd()
+
 # Simulation Sample
 set.seed(63130)
 N <- 10**6 # Population size
@@ -32,6 +37,7 @@ pop <- data.frame(y, x, v0, v1)
 
 # Monte Carlo Simulation {{{
 mc_results <- data.frame()
+cl <- makeCluster(detectCores())
 for (n in ns) {
   for (i in 1:mc) {
     idx <- sample(N, n, replace=FALSE)
@@ -71,7 +77,7 @@ for (n in ns) {
     Q_min <- optim(par=c(0, 0, 0), fn=Q, eta=eta, x=xn, v0=v0n, v1=v1n,
       # lower=c(-2, 0, -2), upper=c(2, 2, 2), 
       method='BFGS',
-      control=list(maxit=500, trace=6))
+      control=list(maxit=500))
 
     # Print Estimation results
     print(round(Q_min$par, 2))
@@ -88,14 +94,12 @@ for (n in ns) {
     )
 
     # Get set C which Q(C) =< min(Q)
-    cl <- makeCluster(detectCores())
     clusterExport(cl, varlist=ls())
     C_star <- candidates[
       parallel::parApply(cl=cl, candidates, 
         FUN=function(c){(Q(xn, v0n, v1n, c, eta) <= (Q_min$value + epsilon_N))}, 
         MARGIN=1)
       , ]
-    stopCluster(cl)
 
     temp <- cbind.data.frame(
       sapply(C_star, max),
@@ -104,120 +108,127 @@ for (n in ns) {
       mc = i,
       n = n
     )
-    mc_results(mc_results, temp)
+    mc_results <- rbind(mc_results, temp)
   }
 }
+stopCluster(cl)
 # }}}
 
-### Graph -- holding other coef fixed {{{
-beta <- expand.grid(
- lapply(Q_min$par, FUN=function(x){seq(x - 2, x + 2, 0.2)})
-)
-Qt <- numeric(length(beta))
-varlist <- c('intercept', 'v', 'x')
-par(mfrow=c(1, 3))
-for (j in 1:3) {
-  gt <- gamma
-  for (i in 1:length(beta)) {
-    gt[j] <- beta[i]
-    Qt[i] <- Q(x, v0, v1, gt, eta)
-  }
-  plot(x=beta, y=Qt, main=paste0('beta_', varlist[j]), ylim=c(0, 5))
-  abline(h=(Q_min$value + epsilon_N), col='red', lty=3)
-  abline(v=gamma[j], lty=2)
-  abline(v=Q_min$par[j], lty=2, col='blue')
-}
-### }}}
+write_excel_csv(mc_results, file=paste0(path, '/data/mc_results.csv'), na='')
+
+mc_results %>%
+  group_by(var, n) %>%
+  summarize(across(everything(), \(x) quantile(x, probs=0.95)), .groups='drop') %>%
+  write_excel_csv(file=paste0(path, '/data/95quantile.csv'), na='')
+
+mc_results %>%
+  group_by(var, n) %>%
+  summarize(across(everything(), mean), .groups='drop') %>%
+  write_excel_csv(file=paste0(path, '/data/mean.csv'), na='')
+
+# ### Graph -- holding other coef fixed {{{
+# beta <- expand.grid(
+#  lapply(Q_min$par, FUN=function(x){seq(x - 2, x + 2, 0.2)})
+# )
+# Qt <- numeric(length(beta))
+# varlist <- c('intercept', 'v', 'x')
+# par(mfrow=c(1, 3))
+# for (j in 1:3) {
+#   gt <- gamma
+#   for (i in 1:length(beta)) {
+#     gt[j] <- beta[i]
+#     Qt[i] <- Q(x, v0, v1, gt, eta)
+#   }
+#   plot(x=beta, y=Qt, main=paste0('beta_', varlist[j]), ylim=c(0, 5))
+#   abline(h=(Q_min$value + epsilon_N), col='red', lty=3)
+#   abline(v=gamma[j], lty=2)
+#   abline(v=Q_min$par[j], lty=2, col='blue')
+# }
+# ### }}}
 
 
-summary(C_star)
-
-
-
-
-# Another Approach -- Find Vertices
-
-## Population 
-get_Cstar <- function(candidates, x, v0, v1, eta) {
-  # Function to check if Q1(c, eta) == 0 and Q0(c, eta) == 0
-  is_in_Cstar <- function(c) {
-    params <- as.numeric(c)
-    f1 <- params[1]*1 + params[2]*v1 + params[3]*x
-    Q1 <- (f1 - eta)**2 * (f1 < eta)
-    
-    f0 <- params[1]*1 + params[2]*v0 + params[3]*x
-    Q0 <- (f0 - eta)**2 * (f0 > eta)
-    
-    # Check if both Q1 and Q0 are zero
-    all(Q1 == 0) && all(Q0 == 0)
-  }
-  
-  # Apply the condition to all candidates
-  Cstar <- candidates[apply(candidates, 1, is_in_Cstar), ]
-  return(Cstar)
-}
-
-# Example usage
-candidates <- expand.grid(
-  lapply(Q_min$par, FUN=function(x) { seq(x - 2, x + 2, 0.01) })
-)
-summary(candidates)
-
-C_star <- get_Cstar(candidates, x, v0, v1, eta)
-dim(C_star)
-head(C_star)
-summary(C_star)
-
-## Sample analogy
-get_Cstar <- function(candidates, x, v0, v1, eta, epsilon_N) {
-  # Function to check if the constraints are satisfied
-  is_in_Cstar <- function(c) {
-    params <- as.numeric(c)
-    
-    # Compute f(x, v0, c) and f(x, v1, c)
-    f0 <- params[1]*1 + params[2]*v0 + params[3]*x
-    f1 <- params[1]*1 + params[2]*v1 + params[3]*x
-    
-    # Check the constraints for all n
-    all((f0 <= (eta + epsilon_N)) & ((eta - epsilon_N) <= f1))
-  }
-  
-  # Apply the condition to all candidates
-  Cstar <- candidates[apply(candidates, 1, is_in_Cstar), ]
-  return(Cstar)
-}
-
-# Example usage
-candidates <- expand.grid(
-  lapply(Q_min$par, FUN=function(x) { seq(x - 1, x + 1, 0.01) })
-)
-
-# Compute C_star using the new constraints
-C_star <- get_Cstar(candidates, x, v0, v1, eta, epsilon_N)
-print(C_star)
-summary(C_star)
-
-plt1 <- C_star[C_star$Var1 == unique(C_star$Var1)[1], ]
-
-# Plot points with Var1 as x and Var2 as y
-par(mfrow=c(1,1))
-plot(C_star$Var2, C_star$Var3, 
-     xlab = "V", 
-     ylab = "X", 
-     # main = "Plot of Var1 vs Var2", 
-     pch = 19, # Solid circle for points
-     col = "blue") # Color of points
-
-
-plt2 <- C_star[C_star$Var3 == unique(C_star$Var3)[2], ]
-
-# Plot points with Var1 as x and Var2 as y
-plot(plt2$Var1, plt2$Var2, 
-     xlab = "Var1", 
-     ylab = "Var2", 
-     main = "Plot of Var1 vs Var2", 
-     pch = 19, # Solid circle for points
-     col = "blue") # Color of points
+# # Another Approach -- Find Vertices
+# ## Population 
+# get_Cstar <- function(candidates, x, v0, v1, eta) {
+#   # Function to check if Q1(c, eta) == 0 and Q0(c, eta) == 0
+#   is_in_Cstar <- function(c) {
+#     params <- as.numeric(c)
+#     f1 <- params[1]*1 + params[2]*v1 + params[3]*x
+#     Q1 <- (f1 - eta)**2 * (f1 < eta)
+#
+#     f0 <- params[1]*1 + params[2]*v0 + params[3]*x
+#     Q0 <- (f0 - eta)**2 * (f0 > eta)
+#
+#     # Check if both Q1 and Q0 are zero
+#     all(Q1 == 0) && all(Q0 == 0)
+#   }
+#
+#   # Apply the condition to all candidates
+#   Cstar <- candidates[apply(candidates, 1, is_in_Cstar), ]
+#   return(Cstar)
+# }
+#
+# # Example usage
+# candidates <- expand.grid(
+#   lapply(Q_min$par, FUN=function(x) { seq(x - 2, x + 2, 0.01) })
+# )
+# summary(candidates)
+#
+# C_star <- get_Cstar(candidates, x, v0, v1, eta)
+# dim(C_star)
+# head(C_star)
+# summary(C_star)
+#
+# ## Sample analogy
+# get_Cstar <- function(candidates, x, v0, v1, eta, epsilon_N) {
+#   # Function to check if the constraints are satisfied
+#   is_in_Cstar <- function(c) {
+#     params <- as.numeric(c)
+#
+#     # Compute f(x, v0, c) and f(x, v1, c)
+#     f0 <- params[1]*1 + params[2]*v0 + params[3]*x
+#     f1 <- params[1]*1 + params[2]*v1 + params[3]*x
+#
+#     # Check the constraints for all n
+#     all((f0 <= (eta + epsilon_N)) & ((eta - epsilon_N) <= f1))
+#   }
+#
+#   # Apply the condition to all candidates
+#   Cstar <- candidates[apply(candidates, 1, is_in_Cstar), ]
+#   return(Cstar)
+# }
+#
+# # Example usage
+# candidates <- expand.grid(
+#   lapply(Q_min$par, FUN=function(x) { seq(x - 1, x + 1, 0.01) })
+# )
+#
+# # Compute C_star using the new constraints
+# C_star <- get_Cstar(candidates, x, v0, v1, eta, epsilon_N)
+# print(C_star)
+# summary(C_star)
+#
+# plt1 <- C_star[C_star$Var1 == unique(C_star$Var1)[1], ]
+#
+# # Plot points with Var1 as x and Var2 as y
+# par(mfrow=c(1,1))
+# plot(C_star$Var2, C_star$Var3, 
+#      xlab = "V", 
+#      ylab = "X", 
+#      # main = "Plot of Var1 vs Var2", 
+#      pch = 19, # Solid circle for points
+#      col = "blue") # Color of points
+#
+#
+# plt2 <- C_star[C_star$Var3 == unique(C_star$Var3)[2], ]
+#
+# # Plot points with Var1 as x and Var2 as y
+# plot(plt2$Var1, plt2$Var2, 
+#      xlab = "Var1", 
+#      ylab = "Var2", 
+#      main = "Plot of Var1 vs Var2", 
+#      pch = 19, # Solid circle for points
+#      col = "blue") # Color of points
 
 
 
