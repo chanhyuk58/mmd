817167f4fe8a56f76c4443bf64e27a896c98231f
Chanhyuk Park <chan.hyuk.58@gmail.com>
2026-03-31 12:22:14 -0500

    2026-03-31 12:22 mc_simulation.R: np to utilize multiple CPUs

--- r_scripts/mc_simulation.R
+++ r_scripts/mc_simulation.R
@@ -1,4 +1,7 @@
 library("txtplot")
+library(parallel)
+options(np.cores = detectCores() - 1)
+
 source("./generate_pop.R")
 source("./mmd_cpp.R")
 
@@ -15,8 +18,13 @@ for (i in 1:mc) {
     obs_start_range = c(50, 60),
   )$data
   # head(pop)
+  cat("Head of population:\n")
+  print(head(pop))
+  cat("Number of Total Civil War Onset:\n")
   print(sum(pop$onset))
+  cat("Proportion of Total Civil War Onset:\n")
   print(mean(pop$onset))
+  cat("Summary of the length of V and the Density plot:\n")
   print(summary(pop$v1 - pop$v0))
   txtdensity((pop$v1 - pop$v0))
 
@@ -40,7 +48,9 @@ for (i in 1:mc) {
     # b_exponent = 0.8,        # Proportion of sample used for bootstrap CI
     verbose = TRUE
   )
+  cat("Summary of Bounds from Pojection Method:\n")
   print(bounds_projection)
+  cat("Summary of Bounds from Grid-search Method:\n")
   print(bounds_grid)
   save(bounds_projection, file = paste("./projection", "_", Sys.Date(), ".rda"))
   save(bounds_grid, file = paste("./grid", "_", Sys.Date(), ".rda"))
